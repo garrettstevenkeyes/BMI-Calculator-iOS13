6e8d9eb52375a912b55890aa94083c4eaf7dbd62
Garrett Keyes <garrett.keyes@gmail.com>
2023-12-28 22:44:13 -0500

    finished bmi app

--- BMI Calculator/Views/CalculatorBrain.swift
+++ BMI Calculator/Views/CalculatorBrain.swift
@@ -6,17 +6,33 @@
 //  Copyright © 2023 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
-    var bmi: Float? = 0.0
+    var bmi: BMI?
     
     mutating func calculateBMI(height:Float, weight:Float) {
-        bmi = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Very fit!", color: UIColor.green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.red)
+        }
     }
     
     func getBMIValue() -> String {
-        let bmiTo1Decimal = String(format: "%.1f", bmi)
+        let bmiTo1Decimal = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTo1Decimal
+    }
+    
+    mutating func getAdvice()-> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    mutating func getColor()-> UIColor {
+        return bmi?.color ?? UIColor.white
     }
 }
